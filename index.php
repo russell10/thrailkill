526d9e223cff7de351f85906b87fb07b497231da
russellthrailkill <thrailkill39@Gmail>
2015-01-28 14:00:00 -0800

    kl

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Website is Running";
+        ?>
+    </body>
+</html>
+
+<style> body, a, a:hover { cursor:url('http://www.dolliehost.com/dolliecrave/cursors/cursors-all/cartoon23.gif'), auto }</style>
+
+
+
+
